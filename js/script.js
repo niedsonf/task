3a04b75024fc3029c4b7c9bdcf6461c03de6814b
Niedson Fernandes <niofernandodf@gmail.com>
2021-06-30 23:25:50 -0300

    add date on cards

--- js/script.js
+++ js/script.js
@@ -24,12 +24,12 @@ class Bd {
         let lembretes = Array()
         let id = localStorage.getItem('id')
 
-        for(let i = 1; i <= id; i++) {
+        for (let i = 1; i <= id; i++) {
             let lembrete = JSON.parse(localStorage.getItem(i))
 
-            if(lembrete === null)
+            if (lembrete === null)
                 continue
-            
+
             lembrete.id = i
             lembretes.push(lembrete)
         }
@@ -45,29 +45,36 @@ class Bd {
 let bd = new Bd()
 
 class Card {
-    constructor(assunto, descricao) {
+    constructor(assunto, descricao, data) {
         this.assunto = assunto
         this.descricao = descricao
+        this.data = data
     }
 
-    criarDOM(id){
+    criarDOM(id, data) {
+        let dataObj = formatarData(data)
         let divExterna = document.createElement('div')
-        divExterna.className = 'card col-12 col-lg-5 mb-2 me-2'
+        divExterna.className = 'card border-dark col-12 col-lg-5 mb-2 me-2'
         let cardHeader = document.createElement('div')
-        cardHeader.className = 'card-header d-flex justify-content-between align-items-center'
+        cardHeader.className = 'card-header d-flex justify-content-between align-items-center bg-warning border-dark'
         let cardBody = document.createElement('div')
         cardBody.className = 'card-body'
+        let cardFooter = document.createElement('div')
+        cardFooter.className = 'card-footer border-dark text-muted'
         let cardTitle = document.createElement('h3')
         cardTitle.className = 'card-title display-6 fs-4'
         cardTitle.innerHTML = this.assunto
         let cardText = document.createElement('h4')
         cardText.className = 'card-text fs-5'
         cardText.innerHTML = this.descricao
+        let cardDate = document.createElement('h4')
+        cardDate.className = 'fs-6 card-text'
+        cardDate.innerHTML = `${dataObj.dia}/${dataObj.mes}/${dataObj.ano} - ${dataObj.diaSemana}`
         let btn = document.createElement('BUTTON')
         btn.id = `lembrete_id_${id}`
-        btn.className = 'btn btn-danger'
+        btn.className = 'btn btn-sm btn-danger text-dark border-dark'
         btn.innerHTML = '<i class="bi bi-x-circle"></i>'
-        btn.onclick = function() {
+        btn.onclick = function () {
             let id = this.id.replace('lembrete_id_', '')
             bd.remover(id)
             window.location.reload()
@@ -75,9 +82,11 @@ class Card {
 
         divExterna.appendChild(cardHeader)
         divExterna.appendChild(cardBody)
+        divExterna.appendChild(cardFooter)
         cardHeader.appendChild(cardTitle)
         cardHeader.appendChild(btn)
         cardBody.appendChild(cardText)
+        cardFooter.appendChild(cardDate)
         let mural = document.getElementById('mural')
         mural.appendChild(divExterna)
     }
@@ -94,20 +103,26 @@ class Card {
 function cadastrar() {
     let assunto = document.getElementById('addModal_assunto')
     let descricao = document.getElementById('addModal_descricao')
+    let data = document.getElementById('addModal_data')
     let statusModal = new bootstrap.Modal(document.getElementById('statusModal'))
     let addModal = new bootstrap.Modal(document.getElementById('addModal'))
+    console.log(data.value)
 
-    let lembrete = new Card(assunto.value, descricao.value)
+    let lembrete = new Card(assunto.value, descricao.value, data.value.replaceAll('-', ','))
 
-    if(lembrete.validarDados()) {
+    if (lembrete.validarDados()) {
         document.getElementById('statusModal_titulo').innerHTML = 'Lembrete salvo com sucesso!'
         document.getElementById('statusModal_btn').innerHTML = 'Adicionar mais'
+        document.getElementById('statusModal_btn').className = 'btn btn-success border-dark'
+        document.getElementById('statusModal_header').className = 'modal-header bg-success border-dark'
         bd.gravar(lembrete)
         assunto.value = ''
         descricao.value = ''
     } else {
         document.getElementById('statusModal_titulo').innerHTML = 'Erro! Preencha todos os campos.'
         document.getElementById('statusModal_btn').innerHTML = 'Voltar e Corrigir'
+        document.getElementById('statusModal_btn').className = 'btn btn-danger border-dark'
+        document.getElementById('statusModal_header').className = 'modal-header bg-danger border-dark'
     }
 
     statusModal.show()
@@ -120,7 +135,42 @@ function carregarLembretes() {
     let lembretes = Array()
     lembretes = bd.recuperarDados()
     lembretes.forEach(l => {
-        let lembrete = new Card(l.assunto, l.descricao)
-        lembrete.criarDOM(l.id);
+        let lembrete = new Card(l.assunto, l.descricao, l.data)
+        lembrete.criarDOM(l.id, l.data);
     })
+}
+
+function formatarData(d) {
+    let data = new Date(d)
+    let dia = data.getDate()
+    let mes = data.getMonth()
+    mes+=1
+    let ano = data.getFullYear()
+    let diaSemana = data.getDay()
+
+    switch(diaSemana) {
+        case 0:
+            diaSemana = 'Domingo'
+            break
+        case 1:
+            diaSemana = 'Segunda'
+            break
+        case 2:
+            diaSemana = 'Terça'
+            break
+        case 3:
+            diaSemana = 'Quarta'
+            break
+        case 4:
+            diaSemana = 'Quinta'
+            break
+        case 5:
+            diaSemana = 'Sexta'
+            break
+        case 6:
+            diaSemana = 'Sábado'
+            break
+    }
+
+    return {dia, mes, ano, diaSemana}
 }
